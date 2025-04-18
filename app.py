ff6ff8500334acabd37be87372f0461a2e6153c7
Ανδρεας Παπαθανασιου <andreaspapathanasiou68@gmail.com>
2025-04-18 05:16:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ add_zero(fig_port_yield)
 
 app = dash.Dash(
     __name__,
-    title="Andreas's Investment Dashboard",
+    title="Andreas's Portfolio Tracker",
     meta_tags=[{"name": "viewport",
                 "content": "width=device-width, initial-scale=1"}],
     external_stylesheets=[dbc.themes.BOOTSTRAP],
